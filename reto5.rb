4cdaaf004a3c704ca2e45261768256be081d85ce
frcharry <fabio.charry@gmail.com>
2019-04-29 22:19:12 -0500

    Adding flow diagram

---
+++ reto5.rb
@@ -0,0 +1,8 @@
+=begin
+Vamos a construir un simple juego de reto 5 en Ruby con la interfaz de la linea
+de comando. El objetivo del juego es que un usuario pueda ingresar la respuesta
+a cada pregunta que le vamos haciendo y que el juego le responda si la respuesta
+ fue correcta o no. En caso de que si sea correcta seguimos con una nueva pregunta,
+ en caso de que no le indicamos al usuario que vuelva a intentar ingresando una
+ nueva respuesta.
+=end
